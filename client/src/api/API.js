000ff400a8aa9362d6bb25dbdd29289823558234
bnGil <gilbn1994@gmail.com>
2022-06-15 22:37:53 +0300

    fix baseURL

--- client/src/api/API.js
+++ client/src/api/API.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-
+const url =
+  process.env.NODE_ENV === "production"
+    ? "https://git.heroku.com/lit-wave-86071.git/weather"
+    : "http://localhost:5000/weather";
 export default axios.create({
-  baseURL: "http://localhost:5000/weather",
+  baseURL: url,
 });
