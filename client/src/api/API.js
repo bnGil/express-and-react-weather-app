b27c2183d1b5438654030ea6ce6a8bc644a8f16e
bnGil <gilbn1994@gmail.com>
2022-06-15 22:15:58 +0300

    fix heroku setup

--- client/src/api/API.js
+++ client/src/api/API.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-
+const url =
+  process.env.NODE_ENV === "production"
+    ? "/weather"
+    : "http://localhost:5000/weather";
 export default axios.create({
-  baseURL: "http://localhost:5050/weather",
+  baseURL: url,
 });
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+console.log("here:", process.env.NODE_ENV);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
