952e8875b1832d7e65b2dc76ba9c92989e0aaaf0
bnGil <gilbn1994@gmail.com>
2022-06-15 21:16:39 +0300

    added scripts to package.json

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.get("/weather/:location", async (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
